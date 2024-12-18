23c4a301166f65f6be304ba46e77264f650f6c23
Sergey <seregaland@yandex.ru>
2024-12-18 13:49:33 +0300

    fix test

--- botrenthub.js
+++ botrenthub.js
@@ -23,6 +23,10 @@ const router = require('./botrenthub/routes/index')
 const sharp = require('sharp');
 const axios = require('axios');
 
+const $host = axios.create({
+    baseURL: process.env.REACT_APP_API_URL
+})
+
 const host = process.env.HOST
 
 let projectId, projectName, projectDate, projectTime, dateStart, manager_id, company_id, Geo, Teh, Worklist, Equipmentlist;
@@ -45,6 +49,7 @@ const socketUrl = process.env.SOCKET_APP_URL
 
 const sendMyMessage = require('./botrenthub/common/sendMyMessage');
 const addMainSpec = require("./botrenthub/common/addMainSpec");
+const sendMessageAdmin = require('./botrenthub/common/sendMessageAdmin')
 
 const chatTelegramId = process.env.CHAT_ID
 const chatGroupId = process.env.CHAT_GROUP_ID
@@ -304,6 +309,55 @@ bot.on('message', async (msg) => {
             } catch (err) {
                 console.error(err.toJSON())
             }
+        }
+        
+        if (text === '/sensmeta ') {
+            try {
+                        //const poster = 'https://proj.uley.team/files/1389/pre/1389_1408579113_customer.pdf'
+                        const poster = `${host}/files/3417/pre/3417_434263815_customer_3.pdf`
+                        console.log("poster API: ", poster)         
+                        //console.log("projectId pred smeta: ", project_id)
+            
+                        //Передаем данные боту
+                        const keyboard = JSON.stringify({
+                            inline_keyboard:[
+                                [{text: 'Подтвердить смету', callback_data:'/smeta '}],
+                                [{text: 'Предложить свою цену', web_app: {url: webAppUrl+'/add-stavka/'}}],
+                            ]
+                        });
+            
+                        console.log("Отправляю постер...")
+                        //const url_send_poster = `https://api.telegram.org/bot${token}/sendDocument?chat_id=${chatId}&document=${poster}&reply_markup=${keyboard}`
+                        const url_send_poster = `https://api.telegram.org/bot${token}/sendDocument?chat_id=${chatId}&document=${poster}`
+                        console.log(url_send_poster)
+            
+                        // создание базы данных "Основной состав"
+                        const response2 = await $host.get(url_send_poster);
+            
+            
+                        //сохранение сметы в базе данных
+                        const convId = await sendMessageAdmin(poster, "image", chatId, response2.data?.result?.message_id, false, 'Подтверждаю')
+                        //console.log("convId: ", convId)
+            
+                        // Подключаемся к серверу socket
+                        let socket = io(socketUrl);
+                        //socket.emit("addUser", chatId)
+            
+                        // //сохранить в контексте (отправка) сметы в админку
+                        socket.emit("sendAdminRent", { 
+                            senderId: chatTelegramId,
+                            receiverId: chatId,
+                            text: poster,
+                            type: 'image',
+                            buttons: 'Подтверждаю',
+                            convId: convId,
+                            messageId: response2.data?.result?.message_id,
+                        })
+            
+                        return res.status(200).json("Poster has been sent successfully");
+                    } catch (error) {
+                        return res.status(500).json(error.message);
+                    }
         }
 
         //------------------------------------------------------------------------------------------------
